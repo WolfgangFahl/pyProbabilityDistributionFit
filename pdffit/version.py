148e241cc687be2003f35ad77ae44cae861b81fd
Wolfgang Fahl <wf@bitplan.com>
2024-08-21 12:17:04 +0200

    fixes version.py

--- pdffit/version.py
+++ pdffit/version.py
@@ -10,8 +10,8 @@ class Version(object):
     Version handling for pyProbabilityDistributionFit
     """
 
-    version = "0.0.5"
+    version = "0.0.6"
     date = "2022-05-18"
-    updated = "2022-07-05"
+    updated = "2024-08-21"
     name = "pyProbabilityDistributionFit"
     description = "Find the best Probability Distribution Function for the given data"
